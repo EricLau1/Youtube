641085f9f61ae595b3db880db6ce629fd8b2e0b9
EricLau1 <ericlau.oliveira@gmail.com>
2020-07-05 19:58:16 -0300

    added go chat with websocket and reactjs

---
+++ go-chat-tutorial/chat/chat.go
@@ -0,0 +1,111 @@
+package chat
+
+import (
+	"fmt"
+	"go-chat-tutorial/utils"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/websocket"
+)
+
+type Chat struct {
+	users    map[string]*User
+	messages chan *Message
+	join     chan *User
+	leave    chan *User
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  512,
+	WriteBufferSize: 512,
+	CheckOrigin: func(r *http.Request) bool {
+		log.Printf("%s %s%s %v\n", r.Method, r.Host, r.RequestURI, r.Proto)
+		return r.Method == http.MethodGet
+	},
+}
+
+func (c *Chat) Handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal("Error on websocket connection:", err.Error)
+	}
+
+	keys := r.URL.Query()
+	username := keys.Get("username")
+	if strings.TrimSpace(username) == "" {
+		username = fmt.Sprintf("anom-%d", utils.GetRandomI64())
+	}
+
+	user := &User{
+		Username: username,
+		Conn:     conn,
+		Global:   c,
+	}
+
+	c.join <- user
+
+	user.Read()
+}
+
+func (c *Chat) Run() {
+	for {
+		select {
+		case user := <-c.join:
+			c.add(user)
+		case message := <-c.messages:
+			c.broadcast(message)
+		case user := <-c.leave:
+			c.disconnect(user)
+		}
+	}
+}
+
+func (c *Chat) add(user *User) {
+	if _, ok := c.users[user.Username]; !ok {
+		c.users[user.Username] = user
+
+		body := fmt.Sprintf("%s join the chat", user.Username)
+		c.broadcast(NewMessage(body, "Server"))
+	}
+}
+
+func (c *Chat) broadcast(message *Message) {
+	log.Printf("Broadcast message: %v\n", message)
+	for _, user := range c.users {
+		user.Write(message)
+	}
+}
+
+func (c *Chat) disconnect(user *User) {
+	if _, ok := c.users[user.Username]; ok {
+		defer user.Conn.Close()
+		delete(c.users, user.Username)
+
+		body := fmt.Sprintf("%s left the chat", user.Username)
+		c.broadcast(NewMessage(body, "Server"))
+	}
+}
+
+func Start(port string) {
+
+	log.Printf("Chat listening on http://localhost:%s\n", port)
+
+	c := &Chat{
+		users:    make(map[string]*User),
+		messages: make(chan *Message),
+		join:     make(chan *User),
+		leave:    make(chan *User),
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Go Webchat!"))
+	})
+
+	http.HandleFunc("/chat", c.Handler)
+
+	go c.Run()
+
+	log.Fatal(http.ListenAndServe(port, nil))
+}
